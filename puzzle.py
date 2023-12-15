6f8fedf92dd5668774580a235edc176d3f6a01b3
Cristina <clmbjcm@gmail.com>
2023-12-15 08:55:15 -0300

    changes

--- puzzle.py
+++ puzzle.py
@@ -50,7 +50,27 @@ def tribonacci(orden, digit):
         return 2025
     """ 
     Matriz de transformación que se obtiene al observar
-    la relación de recurrencia que define la secuncia de Tribonacci
+    la relación de recurrencia que define la secuencia de Tribonacci
+    F(n) = F(n-1)+F(n-2)+F(n-3)
+    Esta relación se puede expresar de manera matricial 
+    utilizando un vector de columnas que contiene 
+    los términos de la secuencia:
+          ┌───   ───┐ 
+          │  F(n)   │ 
+    Xn =  │ F(n-1)  │ 
+          │ F(n-2)  │ 
+          └───   ───┘ 
+    La relación de recurrencia se puede expresar
+    como una multiplicación de matrices:
+        Xn = A x Xn-1
+    
+    ┌───   ───┐     ┌───  ───┐    ┌───               ───┐
+    │  F(n)   │     │ 1 1 1  │    │  F(n)+F(n-1)+F(n-2) │
+    │ F(n-1)  │  X  │ 1 0 1  │  = │        F(n)         │
+    │ F(n-2)  │     │ 0 1 0  │    │       F(n-1)        │
+    └───   ───┘     └───  ───┘    └───               ───┘
+
+
     
     """
     matrix_transf = [[1, 1, 1], [1, 0, 0], [0, 1, 0]]
